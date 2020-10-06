3743ba5128dba0131afd5af19716fca112357428
Michelle McConville <mchelmconvl@gmail.com>
2020-10-05 20:26:40 -0500

    initial commit

---
+++ scripts/questions.js
@@ -0,0 +1,32 @@
+var questionBank = [
+    {   
+        question: "What is 1 + 1?", 
+        selection: ["5","7","2","0"], 
+        answer: "2"
+    },
+
+    {   
+        question: "What is 5 + 5?", 
+        selection: ["Yes","No","Maybe"], 
+        answer: "Maybe"
+    },
+
+    {   
+        question: "Does 0 = 0?", 
+        selection: ["true","false"], 
+        answer: "true"
+    },
+
+];
+
+console.log(questionBank[0].question);
+console.log(questionBank[0].selection);
+console.log(questionBank[0].answer);
+
+console.log(questionBank[1].question);
+console.log(questionBank[1].selection);
+console.log(questionBank[1].answer);
+
+console.log(questionBank[2].question);
+console.log(questionBank[2].selection);
+console.log(questionBank[2].answer);
