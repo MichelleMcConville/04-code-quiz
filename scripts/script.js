215f24c69d214a8c016effdc33fb93f152f715b5
Michelle McConville <mchelmconvl@gmail.com>
2020-10-07 14:19:19 -0500

    fixed high score loop

--- scripts/script.js
+++ scripts/script.js
@@ -1,184 +1,198 @@
 // Global variables, questions are stored in questions.js
-var beginBtn = document.querySelector("#beginBtn");
-var leaderBtn = document.querySelector("#leaderBtn")
+var beginQuiz = document.querySelector("#beginBtn");
+var leaderBtn = document.querySelector("#leaderBtn");
 var timerDisplay = document.querySelector(".timer");
 var gameCard = document.querySelector("#gameCard");
 var question = document.querySelector("#question");
-var mcOne = document.querySelector("#mcOne");
-var mcTwo = document.querySelector("#mcTwo");
-var mcThree = document.querySelector("#mcThree");
-var mcFour = document.querySelector("#mcFour");
+var mcA = document.querySelector("#mcA");
+var mcB = document.querySelector("#mcB");
+var mcC = document.querySelector("#mcC");
+var mcD = document.querySelector("#mcD");
 var answer = document.querySelector("#answer");
 var feedback = document.querySelector("#feedback");
-var card = document.querySelector("#mc");
-var input = document.querySelector("#inputContainer");
+var card = document.querySelector("#multipleChoice");
+var inputForm = document.querySelector("#inputForm");
 var scoreCard = document.querySelector("#scoreCard");
-var scoreBtn = document.querySelector("#scoreBtn");  // might delete this
+var scoreBtn = document.querySelector("#scoreBtn");
 var initialsBox = document.querySelector("#initialsBox");
 var submitBtn = document.querySelector("#submitBtn");
 var backBtn = document.querySelector("#backBtn");
 var clearBtn = document.querySelector("#clearBtn");
+var start = document.querySelector(".start");
 
-
-var timeLeft = 15;
+var timeLeft = questionBank.length * 15;
 var q = 0;
 var s = 0;
 var score = 0;
 var scoreList = [];
+var timeInterval;
 
 getScore();
 // -----------------------------------------------------------------------------
 
-// Timer f(x)
+// Running the timer for the quiz
 function timer() {
-    var timeInterval = setInterval(function() {
-      timeLeft--;
-      timerDisplay.textContent = "TIMER: " + timeLeft;
-      
-      if (timeLeft === 0 || q === questionBank.length) {
-        timerDisplay.textContent = "!! Times Up !!";
-        clearInterval(timeInterval);
-        gameOver();
-      }
-    }, 1000);
-  };
-  // -----------------------------------------------------------------------------
-  
-// Starting the Game f(x)
-// Do I need this if so where here or below displayQA f(x)
-//function startGame(){};
+  timeInterval = setInterval(function () {
+    timeLeft--;
+    timerDisplay.textContent = "TIMER: " + timeLeft;
+
+    if (timeLeft === 0 || q >= questionBank.length) {
+      clearInterval(timeInterval);
+      gameOver();
+    }
+  }, 1000);
+}
 // -----------------------------------------------------------------------------
 
-// Display questions & answers from questionBank
+// Displaying questions & answers from questionBank
 function displayQA() {
-  if(q < questionBank.length) {
+  if (q < questionBank.length) {
     question.textContent = questionBank[q].question;
-    mcOne.textContent = questionBank[q].selection[0];
-    mcTwo.textContent = questionBank[q].selection[1];
-    mcThree.textContent = questionBank[q].selection[2];
-    mcFour.textContent = questionBank[q].selection[3];
+    mcA.textContent = questionBank[q].selection[0];
+    mcB.textContent = questionBank[q].selection[1];
+    mcC.textContent = questionBank[q].selection[2];
+    mcD.textContent = questionBank[q].selection[3];
   } else {
     gameOver();
   }
-};
+}
 // -----------------------------------------------------------------------------
 
-// Showing right or wrong f(x)
-function compareAnswer (event) {
-  console.log(event);
-  if(event === questionBank[q].answer) {
-    score += 10;
-      console.log("You are correct!");
-    feedback.textContent = ("You are correct!");
-      console.log(score);
+// Informing player if chosen answer is right or wrong
+function compareAnswer(event) {
+  if (q >= questionBank.length) {
+    gameOver();
+    clearInterval(timeInterval);
   } else {
-    timeLeft -= 10;
-      console.log("You are Wrong!");
-    feedback.textContent = ("You are Wrong!");
+    if (event === questionBank[q].answer) {
+      feedback.textContent = "You are correct!";
+    } else {
+      timeLeft -= 10;
+      feedback.textContent = "You are Wrong!";
+    }
+    score = timeLeft;
+    q++;
+    displayQA();
   }
-  q++;
-  displayQA();
-};
+}
 // -----------------------------------------------------------------------------
 
+// Getting scores from local storage
 function getScore() {
   var storedScore = JSON.parse(localStorage.getItem("highScore"));
-  if(storedScore !== null) {
+  if (storedScore !== null) {
     scoreList = storedScore;
   }
-};
+}
 // -----------------------------------------------------------------------------
 
+// Saving the scores to local storage
 function saveScore() {
   localStorage.setItem("highScore", JSON.stringify(scoreList));
-};
+}
 // -----------------------------------------------------------------------------
 
-// Game over f(x)
+// Displaying & hiding page items based on Game Over
 function gameOver() {
-  scoreBtn.innerHTML = (score);
+  scoreBtn.innerHTML = score;
   scoreBtn.style.display = "inline-block";
-  //input = "inline-block";
-  //scoreCard = "inline-block";
+  gameCard.classList.add("hide");
+  inputForm.classList.remove("hide");
+  timerDisplay.classList.add("hide");
+  leaderBtn.classList.add("hide");
   leaderBoard();
-};
+}
 // -----------------------------------------------------------------------------
 
-// Keeping track of top 4 leaders w/ local storage f(x)
+// Keeping track of top 10 leaders from local storage w/ loop
 function leaderBoard() {
+  removeFromLeaderBoard();
+  addToLeaderBoard();
   scoreList.sort((a, b) => {
     return b.score - a.score;
   });
   //only render the top 4 scores.
-  topFour = scoreList.slice(0, 4);
+  topTen = scoreList.slice(0, 10);
 
-  if(s < topFour.length) {
-    lbOne.textContent = topFour[0].player + " - " + topFour[0].score;
-    lbTwo.textContent = topFour[1].player + " - " + topFour[1].score;
-    lbThree.textContent = topFour[2].player + " - " + topFour[2].score;
-    lbFour.textContent = topFour[3].player + " - " + topFour[3].score;
+  for (var i = 0; i < topTen.length; i++) {
+    var player = topTen[i].player;
+    var score = topTen[i].score;
+
+    var newDiv = document.createElement("div");
+    leaderBoardDiv.appendChild(newDiv);
+
+    var newLabel = document.createElement("label");
+    newLabel.textContent = player + " - " + score;
+    newDiv.appendChild(newLabel);
   }
-  //console.log(topFour[0].player);
-  //console.log(topFour[0].score);
-};
+}
 // -----------------------------------------------------------------------------
 
-// Go back f(x)
-//function goBack() {};
+// Adding player initials to leader board
+function addToLeaderBoard() {
+  leaderBoardDiv = document.createElement("div");
+  leaderBoardDiv.setAttribute("id", "playerInitials");
+  document.getElementById("leaderBoard").appendChild(leaderBoardDiv);
+}
 // -----------------------------------------------------------------------------
 
-// Reset scores f(x)
-//function resetHighScores () {};
-// -----------------------------------------------------------------------------
-
-// Scoring f(x)
-//function score() {};
-// -----------------------------------------------------------------------------
-
-// Hides start screen and displays question screen
-//function hiddenShow() {};
+// Removing player initials from leader board
+function removeFromLeaderBoard() {
+  var removeScores = document.getElementById("playerInitials");
+  if (removeScores !== null) {
+    removeScores.remove();
+  } else {
+  }
+}
 // -----------------------------------------------------------------------------
 
 // Event listeners
-beginBtn.addEventListener("click", function(event) {
+beginQuiz.addEventListener("click", function (event) {
   timer();
   displayQA();
-  beginBtn.style.display = "none";
-  gameCard.style.display = "block";
+  start.classList.add("hide");
+  gameCard.classList.remove("hide");
+  leaderBtn.style.display = "none";
+  scoreCard.classList.add("hide");
 });
 
-card.addEventListener("click", function(event) {
+card.addEventListener("click", function (event) {
   var event = event.target;
   compareAnswer(event.textContent.trim());
 });
 
-submitBtn.addEventListener("click", function(event) {
-  //console.log("submitBtnClick")
+submitBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  //scoreCard.style.display = "none";  //not sure this is right
-
   var playerInitials = initialsBox.value.trim();
   var newScore = {
     player: playerInitials,
-    score: score
+    score: score,
   };
-  console.log(newScore);
+  
   scoreList.push(newScore);
   saveScore();
+  leaderBoard();
+  inputForm.classList.add("hide");
+  scoreCard.classList.remove("hide");
+});
+
+leaderBtn.addEventListener("click", function (event) {
+  scoreCard.classList.remove("hide");
+  leaderBtn.classList.add("hide");
+  start.classList.add("hide");
+  leaderBoard();
 });
 
 // Event listener for go back button ??
-backBtn.addEventListener("click", function(event) {
-  timer();
-  displayQA();
-  beginBtn.style.display = "inline-block";
-  gameCard.style.display = "none";
-  scoreCard.style.display = "none"
+backBtn.addEventListener("click", function (event) {
+  location.reload();
 });
 
 // Event listener for clear scores button ??
-clearBtn.addEventListener("click", function(event) {
+clearBtn.addEventListener("click", function (event) {
   scoreList = [];
-  saveScore();
+  start.classList.add("hide");
+  localStorage.setItem("highScore", JSON.stringify(scoreList));
   leaderBoard();
+  saveScore();
 });
